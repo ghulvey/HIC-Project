e0f768a4f0177f193091a5638a4c6fe9f3faab3a
Nathan Fleet <nfleet@kent.edu>
2023-04-17 18:44:19 -0400

    transfer done, navbar responsive

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-
-
-
 const Navbar = () => {
-
     const router = useRouter()
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     function logoutAPI() {
         
@@ -33,11 +35,12 @@ const Navbar = () => {
             <span className="font-semibold text-xl tracking-tight">Centralized Crypto</span>
             </div>
             <div className="block lg:hidden">
-            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+                    onClick={toggleMenu}>
                 <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
             </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className={`w-full block lg:flex lg:items-center lg:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
             <div className="text-right lg:flex-grow">
                 <a href="" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
                 Dashboard
@@ -50,6 +53,9 @@ const Navbar = () => {
                 </a>
                 <a href="" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
                 Withdraw
+                </a>
+                <a href="/transfer" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                Transfer
                 </a>
                 <button onClick={logoutAPI} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-red-900 hover:bg-white mt-4 lg:mt-0">Logout</button>
             </div>
